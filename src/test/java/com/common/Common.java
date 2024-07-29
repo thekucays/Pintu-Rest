069411f1e7bec294657a850e3d5450f443e3b119
thekucays <luki.ramadon@bfi.co.id>
2024-07-29 19:57:31 +0700

    refactor: move schema file to schema folder

--- src/test/java/com/common/Common.java
+++ src/test/java/com/common/Common.java
@@ -1,16 +1,18 @@
 package com.common;
 
 import org.testng.Assert;
-
+import java.io.File;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 
 public class Common {
 	public static void getGenderResponse(String name, String expected) {
 		final String genderURL = "https://api.genderize.io/?name=" + name;
 		Response apiResponse = get(genderURL);
+		
 		
 		// assert things 
 		Assert.assertEquals(apiResponse.getStatusCode(), 200);
@@ -23,15 +25,18 @@ public class Common {
 	
 	public static void getPost() {
 		final String getPostURL = "https://jsonplaceholder.typicode.com/posts";
+		final File schemaFile = new File("schemas/getPostSchema.json");
+		
 		// get new post and assert result
 		given().get(getPostURL)
 			.then().statusCode(200)
-			.and().assertThat().body(matchesJsonSchemaInClasspath("getPostSchema.json"));
+			.and().assertThat().body(JsonSchemaValidator.matchesJsonSchema(schemaFile));
 		
 	}
 	
 	public static void createPost(String title, String body, int userId) {
         final String createPostURL = "https://jsonplaceholder.typicode.com/posts";
+        final File schemaFile = new File("schemas/createPostSchema.json");
         
         // Define the post body
         String postBody = String.format("{ \"title\": \"%s\", \"body\": \"%s\", \"userId\": %d }", title, body, userId);
@@ -45,7 +50,7 @@ public class Common {
             .then()
             .statusCode(201)  // 201 Created is the expected status code for successful POST requests
             .and()
-            .assertThat().body(matchesJsonSchemaInClasspath("createPostSchema.json"))
+            .assertThat().body(JsonSchemaValidator.matchesJsonSchema(schemaFile))
             .and()
             .assertThat().body("title", equalTo(title))
             .and()
