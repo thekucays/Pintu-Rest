81b97725091cb35e9c6e76ec51da6454eda3ca45
thekucays <luki.ramadon@bfi.co.id>
2024-07-29 19:59:21 +0700

    chore: remove unused things

--- src/test/java/com/common/Common.java
+++ src/test/java/com/common/Common.java
@@ -1,28 +1,11 @@
 package com.common;
 
-import org.testng.Assert;
 import java.io.File;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import io.restassured.module.jsv.JsonSchemaValidator;
-import io.restassured.response.Response;
 
 public class Common {
-	public static void getGenderResponse(String name, String expected) {
-		final String genderURL = "https://api.genderize.io/?name=" + name;
-		Response apiResponse = get(genderURL);
-		
-		
-		// assert things 
-		Assert.assertEquals(apiResponse.getStatusCode(), 200);
-		given().get(genderURL)
-			.then().statusCode(200)
-			.and().body("gender", equalTo(expected))
-			.and().assertThat().body(matchesJsonSchemaInClasspath("genderSchema.json"));
-	}
-	
-	
 	public static void getPost() {
 		final String getPostURL = "https://jsonplaceholder.typicode.com/posts";
 		final File schemaFile = new File("schemas/getPostSchema.json");
